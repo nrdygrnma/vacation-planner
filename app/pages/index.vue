10b92736b88bce6e32192ecab568fe04197ebce8
nrdygrnma <github@nrdygrnma.tech>
2025-10-27 19:16:55 +0100

    Defined flyonui and set themes

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1 class="text-3xl text-orange-500 font-light">Vacation Planner</h1>
+    <h1 class="text-3xl text-primary font-light">Vacation Planner</h1>
 </template>
 
 <script lang="ts" setup>
